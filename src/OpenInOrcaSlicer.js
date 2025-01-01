3c4bdc48f705c59d5cc2f0fdfe8bce36b47abca9
Keziah Biermann <development@kizzycode.de>
2025-01-01 23:37:37 +0100

    Add Update URL

--- src/OpenInOrcaSlicer.js
+++ src/OpenInOrcaSlicer.js
@@ -3,8 +3,8 @@
 // @description Replaces the "prusaslicer://open?file="-URLs with OrcaSlicer compatible URLs
 // @match       https://www.printables.com/model/*
 // @license     BSD-2-Clause OR MIT
-// @downloadURL 
-// @updateURL   
+// @downloadURL https://raw.githubusercontent.com/KizzyCode/Printables-OpenInOrcaSlicer-UserScript/refs/heads/master/src/OpenInOrcaSlicer.js
+// @updateURL   https://raw.githubusercontent.com/KizzyCode/Printables-OpenInOrcaSlicer-UserScript/refs/heads/master/src/OpenInOrcaSlicer.js
 // ==/UserScript==
 
 (function() {
